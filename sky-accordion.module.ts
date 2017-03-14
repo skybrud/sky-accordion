79400eb34e7fee44398cf21a5985bc99ea4b304a
Rudi Ørnhøj <rornhoj@gmail.com>
2017-03-14 11:42:14 +0100

    Delete old files

--- sky-accordion.module.ts
+++
@@ -1,8 +0,0 @@
-/* global angular */
-(function () {
-	'use strict';
-
-	angular.module('skyAccordion',['core']);
-	angular.module('skyAccordion').constant('skyAccordionTopScrollOffset', 75);
-
-})();
