060fdce7b4379c1596ed338ab29c57e16e33727c
Rudi Ørnhøj <rornhoj@gmail.com>
2018-09-10 20:53:18 +0200

    Removed vue from bus part

--- src/SkyAccordionStore.js
+++ src/SkyAccordionStore.js
@@ -1,34 +1,26 @@
-import Vue from 'vue';
-
-const SkyAccordionStore = new Vue({
-	data() {
-		return {
-			accordions: [],
-		};
+const SkyAccordionStore = (() => ({
+	accordions: [],
+	add(accordion) {
+		const index = this.accordions.indexOf(accordion);
+		if (index === -1) {
+			this.accordions.push(accordion);
+		} else {
+			this.accordions[index] = accordion;
+		}
 	},
-	methods: {
-		add(accordion) {
-			const index = this.accordions.indexOf(accordion);
-			if (index === -1) {
-				this.accordions.push(accordion);
-			} else {
-				this.accordions[index] = accordion;
-			}
-		},
-		remove(accordion) {
-			const index = this.accordions.indexOf(accordion);
-			if (index > -1) {
-				this.accordions.splice(index, 1);
-			}
-		},
-		toggleAll(bool, exclude = []) {
-			this.accordions
-				.filter(acc => exclude.indexOf(acc) === -1)
-				.forEach((acc) => {
-					acc.$set(acc, 'isOpen', false);
-				});
-		},
+	remove(accordion) {
+		const index = this.accordions.indexOf(accordion);
+		if (index > -1) {
+			this.accordions.splice(index, 1);
+		}
 	},
-});
+	toggleAll(bool, exclude = []) {
+		this.accordions
+			.filter(acc => exclude.indexOf(acc) === -1)
+			.forEach((acc) => {
+				acc.$set(acc, 'isOpen', false);
+			});
+	},
+}))();
 
 export default SkyAccordionStore;
